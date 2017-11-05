57d661ac85c0192e6d37a5b76fad71a69c47a3b4
JustHitIT <33395919+JustHitIT@users.noreply.github.com>
2017-11-05 17:45:46 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = '-';
+
+client.on('ready', () => {
+  console.log(`Bot ${client.user.username} adi ile giris yapti!`);
+});
+
+client.on('message', msg => {
+  if (!msg.content.startsWith(prefix)) {
+	  return;
+  }
+  if (msg.content === prefix + 'ping') {
+    msg.reply('Pong! **' + client.ping + '** ms');
+  }
+  if (msg.content === prefix + 'kanal') {
+    msg.channel.sendMessage('JustHitIT Resmi Bot!');
+  }
+  if (msg.content === prefix + 'youtube') {
+    msg.channel.sendMessage('Kanal: JustHitIT!');
+  }
+  if (msg.content === prefix + 'help') {
+    msg.channel.sendMessage('Komutlar');
+	msg.channel.sendMessage('kanal');
+	msg.channel.sendMessage('ping');
+	msg.channel.sendMessage('youtube');
+  }
+});
+
+client.login('Mzc2NzE3NTgxODk5NTMwMjQx.DOCdHw.VUgZJnoBAwPF3lVNj5h2fN980es');
